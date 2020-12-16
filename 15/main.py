d9a411bc8e77a4df51c8bbbd00e8ddf6516fa75e
jamegwsun <jame.sun@formlabs.com>
2020-12-16 02:01:25 -0500

    clean up day 15

--- 15/main.py
+++ 15/main.py
@@ -1,7 +1,6 @@
-# with open('input') as f:
-#     start = [int(l) for l in f.split('')]  # fix this
+with open('input') as f:
+    start = [int(l) for l in f.read().split(',')]
 
-start = [14,3,1,0,9,5]
 age = dict()
 last_num, curr_num = None, None
 
@@ -9,11 +8,12 @@ for i in range(30000000):
     if i < len(start):
         curr_num = start[i]
     else:
-        curr_num =  i - 1 - age[last_num] if last_num in age.keys() else 0
+        curr_num = i - 1 - age[last_num] if last_num in age.keys() else 0
         age[last_num] = i
     if last_num is not None:    
         age[last_num] = i - 1
     last_num = curr_num
-    if i % 100 == 0:
-        print(i)
-print(last_num)
+    if i == 2019:
+        p1_num = last_num
+print('part 1:', p1_num)
+print('part 2:', last_num)
