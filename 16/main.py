fd6b641f60e2497ef4a700b36958cf57b87c38ef
jamegwsun <jame.sun@formlabs.com>
2020-12-16 23:49:32 -0500

    cleaned up day 16

--- 16/main.py
+++ 16/main.py
@@ -1,59 +1,40 @@
 import re
-
-# TODO rewrite part 1, clean the hell up
-
-my_ticket = tuple()
-dpt_pos, all_rules, nearby_tickets = [], [], []
+import numpy as np
 
 
-def check_subrules(n: int, r: list):
-    for sr in r:
-        if n in sr:
-            return True
-
+dpt_indices, rules, nearby_tickets, score = [], [], [], []
+is_mine = True
 
 with open('input') as f:
-    is_mine = True
     for i, l in enumerate(f):
         r = re.findall('(\d+)-(\d+)', l)
         if r:
             if 'departure' in l:
-                dpt_pos.append(i)
-            all_rules.append([range(int(l), int(h) + 1) for l, h in r] + [i])
+                dpt_indices.append(i)
+            rules.append([range(int(l), int(h) + 1) for l, h in r] + [i])
             continue
-        elif 'nearby' in l:
+        if 'nearby' in l:
             is_mine = False
-
         t = re.findall('(\d+)', l)
         if t:
             if is_mine:
                 my_ticket = t
             else:
-                for n in t:
-                    is_valid = False
-                    for r in all_rules:
-                        for sr in r[:-1]:
-                            if int(n) in sr:
-                                is_valid = True
-                    if not is_valid:
-                        break
-                if is_valid:
+                invalids = [int(n) for n in t if all(int(n) not in sr for r in rules for sr in r[:-1])]
+                if invalids:
+                    score += invalids
+                else:
                     nearby_tickets.append(tuple([int(n) for n in t]))
 
+translate = {i: None for i in range(len(rules))}  # rule index : ticket index
 
-convert_rules = {i: None for i in range(len(all_rules))}  # rule index : ticket index
-
-for i in range(20):
-    print('\n\n')
-    for i in range(len(my_ticket)):
+while rules:
+    for i in [t for t in range(len(my_ticket)) if t not in translate.values()]:
         col = [nt[i] for nt in nearby_tickets]
-        match = [(p, r[-1]) for p, r in enumerate(all_rules) if all(check_subrules(n, r[:-1]) for n in col)]
+        match = [(p, r[-1]) for p, r in enumerate(rules) if all(any(n in sr for sr in r[:-1]) for n in col)]
         if len(match) == 1:
-            convert_rules[match[0][1]] = i
-            all_rules.pop(match[0][0])
+            translate[match[0][1]] = i
+            rules.pop(match[0][0])
 
-answer = 1
-for i, v in convert_rules.items():
-    answer *= int(my_ticket[v])
-    if i == 5:
-        print(answer)
+print('part 1:', sum(score))
+print('part 2:', np.prod([int(my_ticket[translate[i]]) for i in dpt_indices]))
